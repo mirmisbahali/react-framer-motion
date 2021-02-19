c630bf243886441f11bbf4a841ad989f4e23ac52
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-19 20:24:48 +0530

    AnimatePresence

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const containerVariants = {
   hidden: {
@@ -9,8 +9,8 @@ const containerVariants = {
   visible: {
     opacity: 1,
     x: 0,
-    transition: { 
-      type: 'spring', 
+    transition: {
+      type: 'spring',
       // delay: 0.5,
       mass: 0.4,
       damping: 8,
@@ -31,13 +31,27 @@ const childVariants = {
 }
 
 const Order = ({ pizza }) => {
+
+  const [showTitle, setShowTitle] = useState(true);
+
+  setTimeout(() => {
+    setShowTitle(false);
+  }, 4000);
+
   return (
     <motion.div className="container order"
       variants={containerVariants}
       initial="hidden"
       animate="visible"
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2
+            exit={{ y: -1000 }}
+          >Thank you for your order :)</motion.h2>
+
+        )}
+      </AnimatePresence>
       <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
       <motion.div variants={childVariants}>
         {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
